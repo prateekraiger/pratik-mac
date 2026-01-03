388e7a7f20fdae745c8c1ebed8169d305257fc70
prateekraiger <prateekraiger123@gmail.com>
2026-01-03 22:33:47 +0530

    feat: introduce new image assets and constants for photo gallery and navigation links

--- src/constants/photos.js
+++ src/constants/photos.js
@@ -1,3 +1,11 @@
+import image1 from "../assets/about/image1.jpg";
+import image2 from "../assets/about/image2.png";
+import image3 from "../assets/about/image3.jpg";
+import image4 from "../assets/about/image4.jpg";
+import image5 from "../assets/about/image5.jpg";
+import image6 from "../assets/about/image6.jpg";
+import image7 from "../assets/about/image7.jpg";
+
 export const photosLinks = [
   {
     id: 1,
@@ -26,4 +34,33 @@ export const photosLinks = [
   },
 ];
 
-export const gallery = [];
+export const gallery = [
+  {
+    id: 1,
+    img: image1,
+  },
+  {
+    id: 2,
+    img: image2,
+  },
+  {
+    id: 3,
+    img: image3,
+  },
+  {
+    id: 4,
+    img: image4,
+  },
+  {
+    id: 5,
+    img: image5,
+  },
+  {
+    id: 6,
+    img: image6,
+  },
+  {
+    id: 7,
+    img: image7,
+  },
+];
